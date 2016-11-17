80bc54a1e3ffba2592639cfd03ac97932341332c
benjamin.saint-sever <saintseverbenjamin@gmail.com>
2016-11-17 23:13:23 +0100

    Avancée sur la reconnaissance vocale ...

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 #include "dtw.h"
 #include "WavToMfcc.h"
+#include "FFTReal.h"
+#include "libmfccOptim.h"
 #include <string>
 #include <cstring>
 #include <stdlib.h>
+#include <cstdlib>
 
 
 using namespace std;
 
+string chemin = "/Users/benjamin.saint-sever/Documents/Education/Master 1.2/Semestre 1/MCS/ProjectMCSPartieC-/corpus/dronevolant_nonbruite/";
+
+int nbmots = 13;
+string vocabulaire[13] = {"arretetoi", "atterrissage", "avance", "decollage", "droite", "etatdurgence", "faisunflip",
+                          "gauche", "plusbas", "plushaut", "recule", "tournedroite", "tournegauche"};
+
+
+/** VARIABLES PARAMETRISATION **/
+struct parametrisation {
+    float *X_mfcc;
+    int length_xmfcc;
+};
+
+
 int main() {
 
+    /*******************************************
+     ***************  TEST DTW  ****************
+     *******************************************/
     float sequence1[9];
     float sequence2[6];
 
@@ -36,90 +56,153 @@ int main() {
     std::cout << std::to_string(res) << std::endl;
 
 
-    /*Seconde partie*/
+    /********************************************
+    ***********  Reconnaisance vocale  **********
+    ********************************************/
 
-    string chemin = "/corpus/dronevolant_nonbruite/";
-    int nbmots = 13;
-    string vocabulaire[nbmots]={"arretetoi", "atterrissage", "avance", "decollage", "droite", "etatdurgence", "faisunflip", "gauche", "plusbas", "plushaut", "recule", "tournedroite", "tournegauche"};
-
+    FILE** refFILE;
+    wavfile header_fichier[nbmots];
+    struct parametrisation ref[1]; //1 le nombre de locuteur de ref
     string locuteur = "M01";
-    string nomfichier;
 
-
-    FILE** refFILE [nbmots];
-    wavfile* header_fichier[nbmots];
-
-    char* c_mfcName[nbmots];
-
-    /* VARIABLES PARAMETRISATION */
-    struct parametrisation{
-        float* X_mfcc;
-        int length_xmfcc;
-    };
-    struct parametrisation par;
-
-
-    for (int mot = 0; mot < nbmots; ++mot){
-        nomfichier = chemin + locuteur + "_" + vocabulaire[mot] + ".wav";
-        char* c_nomfichier = new char[nomfichier.length() + 1];
+    /** Fichiers de reférences **/
+    for (int mot = 0; mot < nbmots; ++mot) {
+        string nomfichier = chemin + locuteur + "_" + vocabulaire[mot] + ".wav";
+        char *c_nomfichier = new char[nomfichier.length() + 1];
         strcpy(c_nomfichier, nomfichier.c_str());
 
+
+
         /** LECTURE **/
-        wavRead(refFILE[mot], c_nomfichier, header_fichier[mot]);
+        wavRead(&refFILE[mot], c_nomfichier, &header_fichier[mot]);
 
 
         /* Deplacement du curseur après l'entete wav, directement sur les données (44 octets d'entete) */
-        fseek(refFILE[mot],SEEK_SET, sizeof(struct wavfile));
+        fseek(refFILE[mot], SEEK_SET, sizeof(struct wavfile));
 
 
-        int nbreOctetsAudio = header_fichier[mot]->bytes_in_data;
+        int nbreOctetsAudio = header_fichier[mot].bytes_in_data;
         int nbreOctetsRestant = nbreOctetsAudio;
         char *buffer = new char[nbreOctetsAudio];
         int16_t donnee[nbreOctetsAudio];
         int indice = 0;
 
+
         /** LECTURE ET COPIE DES DONNEES AUDIO **/
-        while(fread(buffer, sizeof(int16_t),1,refFILE[mot]) > 0 && (nbreOctetsRestant > 0)){
+        while (fread(buffer, sizeof(int16_t), 1, refFILE[mot]) > 0 && (nbreOctetsRestant > 0)) {
             donnee[indice] = buffer[indice];
+
             indice++;
             nbreOctetsRestant--;
         }
+        cout<<buffer<<endl;
+
 
         /** REMOVE SILENCE **/
 
-        int16_t* signalSansSilence;
+        int16_t *signalSansSilence;
         int taille_signal;
-        float threshold = 1/100; //Sensibilité de détection du silence
-        removeSilence(donnee,nbreOctetsAudio, &signalSansSilence, &taille_signal, threshold);
+        float threshold = 1 / 100; //Sensibilité de détection du silence
+        removeSilence(donnee, nbreOctetsAudio, &signalSansSilence, &taille_signal, threshold);
 
-
-
-        /* fichier en mfc A quoi cela peut il servir ?
-         * nameWavToMfc(c_nomfichier, c_mfcName);
-         */
 
 
         /** PARAMETRISATION **/
 
         /* IMPLEMENTATION BASE SUR UNE SOURCE */
-        computeMFCC(&par.X_mfcc, &par.length_xmfcc, signalSansSilence, nbreOctetsAudio, header_fichier[mot]->frequency,
-                    512, 256, 13, 26 );
-        //computeMFCC(float **X_mfcc, int *length_xmfcc, int16_t *x, int Nx, int frequency, int frame_length, int frame_step,
-        //               int dim_mfcc, int num_filter);
+        computeMFCC(&ref[0].X_mfcc, &ref[0].length_xmfcc, signalSansSilence, nbreOctetsAudio, header_fichier[mot].frequency,
+                    512, 256, 13, 26);
 
 
 
-        delete [] c_nomfichier;
+
+        delete[] c_nomfichier;
 
     }
 
 
-    string hypotheses[12]={"M02","M03","M04","M05","M06","M07","M08", "M09", "M10", "M11", "M12", "M13"};
+    int nbLocuteur = 12;
+    string hypotheses[nbLocuteur] = {"M02", "M03", "M04", "M05", "M06", "M07", "M08", "M09", "M10", "M11", "M12",
+                                     "M13"};
+
+
+    int nbMotsRec = 0;
+
+    struct parametrisation paramHyp[nbLocuteur];
+    for (int noLocuteur = 0; noLocuteur < nbLocuteur; ++noLocuteur) {
+
+        locuteur = hypotheses[noLocuteur];
+        for (int mot = 1; mot < nbmots; ++mot) {
+
+                                                    FILE **ref_FILE[nbmots];
+                                                    wavfile *head_file[nbmots];
+
+                                                    /** Fichiers de reférences **/
+                                                    for (int mot = 0; mot < nbmots; ++mot) {
+                                                        string nomfichier = chemin + locuteur + "_" + vocabulaire[mot] + ".wav";
+                                                        char *c_nomfichier = new char[nomfichier.length() + 1];
+                                                        strcpy(c_nomfichier, nomfichier.c_str());
+
+                                                        /** LECTURE **/
+                                                        wavRead(ref_FILE[mot], c_nomfichier, head_file[mot]);
+
+
+                                                        /* Deplacement du curseur après l'entete wav, directement sur les données (44 octets d'entete) */
+                                                        fseek(*ref_FILE[mot], SEEK_SET, sizeof(struct wavfile));
+
+
+                                                        int nbreOctetsAudio = head_file[mot]->bytes_in_data;
+                                                        int nbreOctetsRestant = nbreOctetsAudio;
+                                                        char *buffer = new char[nbreOctetsAudio];
+                                                        int16_t donnee[nbreOctetsAudio];
+                                                        int indice = 0;
+
+                                                        /** LECTURE ET COPIE DES DONNEES AUDIO **/
+                                                        while (fread(buffer, sizeof(int16_t), 1, *ref_FILE[mot]) > 0 && (nbreOctetsRestant > 0)) {
+                                                            donnee[indice] = buffer[indice];
+                                                            indice++;
+                                                            nbreOctetsRestant--;
+                                                        }
+
+                                                        /** REMOVE SILENCE **/
+
+                                                        int16_t *signalSansSilence;
+                                                        int taille_signal;
+                                                        float threshold = 1 / 100; //Sensibilité de détection du silence
+                                                        removeSilence(donnee, nbreOctetsAudio, &signalSansSilence, &taille_signal, threshold);
 
 
 
+                                                        /** PARAMETRISATION **/
+
+                                                        /* IMPLEMENTATION BASE SUR UNE SOURCE */
+                                                        computeMFCC(&paramHyp[mot].X_mfcc, &paramHyp[mot].length_xmfcc, signalSansSilence, nbreOctetsAudio,
+                                                                    head_file[mot]->frequency,
+                                                                    512, 256, 13, 26);
 
 
+                                                        delete[] c_nomfichier;
 
-        return 0;
+                                                    }
+
+
+        }
+
+
+        for (int motRef = 0; motRef < nbmots; ++motRef) {
+            //Dmots = zeros(1,nbmots); ?
+            float *dMots = new float[nbmots];
+            for (int motHyp = 0; motHyp < nbmots; ++motHyp) {
+                float distance = dtw(paramHyp[motHyp].length_xmfcc,ref[motRef].length_xmfcc,0,paramHyp[motHyp].X_mfcc,ref[motRef].X_mfcc);
+                dMots[motHyp] = distance;
+            }
+
+
+        }
+
+
+    }
+
+
+    return 0;
 }
